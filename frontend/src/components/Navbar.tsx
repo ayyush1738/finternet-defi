000c322b0a799d1fbeeeca7774aea594967bce7b
unknown <singhrathoreayush824@gmail.com>
2025-07-14 12:05:41 +0530

    Ayush

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,12 +1,12 @@
-import WalletLogin from "./WalletLogin"
+import WalletLogin from "./WalletLogin";
 
 export default function Navbar() {
   return (
-    <div className="absolute top-0 left-0 w-full z-30 flex items-center px-6 py-4">
-      <div className="flex ml-10">
-            <h1 className="text-white text-3xl font-bold mb-4">Cha</h1>
-            <h1 className="text-fuchsia-600 text-3xl font-bold mb-4">inVoice</h1>
-          </div>
+    <div className="absolute top-0 left-0 w-full z-30 flex items-center px-4 py-3 sm:px-6 sm:py-4">
+      <div className="flex flex-col sm:flex-row sm:items-center ml-4 sm:ml-10">
+        <h1 className="text-white text-md sm:text-3xl font-bold leading-tight">Cha</h1>
+        <h1 className="text-fuchsia-600 text-md sm:text-3xl font-bold leading-tight">inVoice</h1>
+      </div>
       <div className="ml-auto">
         <WalletLogin />
       </div>
